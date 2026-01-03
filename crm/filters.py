bb09810ea5bf11121bf263a39f14c7e57938d2b7
lexStack3 <alexanderedim80@gmail.com>
2026-01-03 10:58:47 +0100

    Created CustomerFilter, ProductFilter, and OrderFilter.

---
+++ crm/filters.py
@@ -0,0 +1,40 @@
+import django_filters
+from .models import Customer, Product, Order
+
+
+class CustomerFilter(django_filters.FilterSet):
+    name_icontains = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    email_icontains = django_filters.CharFilter(field_name='email', lookup_expr='icontains')
+    created_at_gte = django_filters.DateFilter(field_name='created_at', lookup_expr='gte')
+    created_at_lte = django_filters.DateFilter(field_name='created_at', lookup_expr='lte')
+    phone_startswith = django_filters.CharFilter(field_name='phone', lookup_expr='startswith')
+
+    
+    class Meta:
+        model = Customer
+        fields = []
+
+
+class ProductFilter(django_filters.FilterSet):
+    name_icontains = django_filters.CharFilter(field_name='name', lookup_expr='icontains')
+    price_gte = django_filters.NumberFilter(field_name='price', lookup_expr='gte')
+    price_lte = django_filters.NumberFilter(field_name='price', lookup_expr='lte')
+    stock_gte = django_filters.NumberFilter(field_name='stock', lookup_expr='gte')
+    stock_lte = django_filters.NumberFilter(field_name='stock', lookup_expr='lte')
+
+    class Meta:
+        model = Product
+        fields = []
+
+
+class OrderFilter(django_filters.FilterSet):
+    total_amount_gte = django_filters.NumberFilter(field_name='total_amount', lookup_expr='gte')
+    total_amount_lte = django_filters.NumberFilter(field_name='total_amount', lookup_expr='lte')
+    order_date_gte = django_filters.DateFilter(field_name='order_date', lookup_expr='gte')
+    order_date_lte = django_filters.DateFilter(field_name='order_date', lookup_expr='lte')
+    customer_name = django_filters.CharFilter(field_name='customer__name', lookup_expr='icontains')
+    product_name = django_filters.CharFilter(field_name='products__name', lookup_expr='icontains')
+
+    class Meta:
+        model = Order
+        fields = []
