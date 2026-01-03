caf4037d44b8c90f376aaf38e1b70be75beda42e
lexStack3 <alexanderedim80@gmail.com>
2026-01-03 09:31:11 +0100

    Created the Customer, Product, and Order models for the project.

--- crm/models.py
+++ crm/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Customer(models.Model):
     """A model representation of a <Cusgtomer> instance."""
-    name = models.CharField(max_length=100, blank=False)
+    name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100, blank=False)
     phone = models.CharField(max_length=15, blank=True, null=True)
 
